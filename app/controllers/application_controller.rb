de54892cb1b705ae8369a48d3a13bdde3a53e8c5
野尻稔彦 <nojiritoshihiko@nojiritoshihiko-no-MacBook-Air.local>
2017-11-25 10:29:33 +0900

    devise view's modification wasn't reflected...

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,8 +6,8 @@ class ApplicationController < ActionController::Base
   # before_actionで下で定義したメソッドを実行
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  #変数PERMISSIBLE_ATTRIBUTESに配列[:name]を代入
-  PERMISSIBLE_ATTRIBUTES = %i(name)
+  #変数PERMISSIBLE_ATTRIBUTESに配列[:name]及びcarrierwave用のカラムを代入
+  PERMISSIBLE_ATTRIBUTES = %i(name avatar avatar_cache)
 
   protected
 
@@ -16,5 +16,5 @@ class ApplicationController < ActionController::Base
       devise_parameter_sanitizer.permit(:sign_up, keys: PERMISSIBLE_ATTRIBUTES)
       devise_parameter_sanitizer.permit(:account_update, keys: PERMISSIBLE_ATTRIBUTES)
     end
-    
+
 end
